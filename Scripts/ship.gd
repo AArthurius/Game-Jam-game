2e316e9efa4d87508ad2ad7705acd4ab8fdbd34a
AArthurius <arthurpennarey@gmail.com>
2024-09-08 16:06:08 -0300

    poggers

--- Scripts/ship.gd
+++ Scripts/ship.gd
@@ -1,31 +1,62 @@
 extends CharacterBody2D
 
-var full_health = preload("res://Assets/Hull/Hull - Full Health.png")
-var slightly_damaged: CompressedTexture2D = preload("res://Assets/Hull/Hull - Slight Damage.png")
-var very_damaged = preload("res://Assets/Hull/Hull - Very damage.png")
-var broken = preload("res://Assets/Hull/Hull - Broken.png")
+var full_health = preload("res://Assets/Ship/Hull/Hull - Broken.png")
+var slightly_damaged: CompressedTexture2D = preload("res://Assets/Ship/Hull/Hull - Slight Damage.png")
+var very_damaged = preload("res://Assets/Ship/Hull/Hull - Very damage.png")
+var broken = preload("res://Assets/Ship/Hull/Hull - Broken.png")
 
-@onready var sprite: Sprite2D = $Hull
+@onready var engine: AnimatedSprite2D = $BaseEngineEffects
 
-var desired_dir := Vector2(0,0)
-var dir := Vector2(0,0)
-var current_speed: int = 0
-var acc:int = 200
-
-const MAX_SPEED: int = 200
-
-func _ready() -> void:
-	#sprite.set_texture(slightly_damaged)
-	pass
+const MAX_SPEED = 350
+var acc = 300
+var input_dir: Vector2 = Vector2(0, 0)
+var direction: Vector2 = Vector2(0, 0)
 
 func _process(delta: float) -> void:
-	desired_dir = Input.get_vector("left", "right", "up", "down")
+	input_dir = Input.get_vector("left","right","up", "down")
 	look_at(get_global_mouse_position())
+	
+	if velocity.length() != 0:
+		engine.play("powering")
+	else:
+		engine.play("idle")
 
 func _physics_process(delta: float) -> void:
-	movement(delta)
-
-func movement(delta):
-	velocity = desired_dir * MAX_SPEED
+	if input_dir:
+		direction += input_dir * acc * delta
+	else:
+		friction(delta)
+	
+	direction.x = clamp(direction.x, -MAX_SPEED, MAX_SPEED)
+	direction.y = clamp(direction.y, -MAX_SPEED, MAX_SPEED)
+	
+	
+	velocity = direction 
+	print(velocity)
 	
 	move_and_slide()
+
+func friction(delta):
+	var friction_x: int = 0
+	var friction_y: int = 0
+	var vel_x: int = direction.x
+	var vel_y: int = direction.y
+	
+	
+	if direction.x > 0:
+		friction_x = acc * delta
+		vel_x -= friction_x
+		direction.x = max(vel_x, 0)
+	if direction.x < 0:
+		friction_x = acc * delta
+		vel_x += friction_x
+		direction.x = min(vel_x, 0)
+	
+	if direction.y > 0:
+		friction_y = acc * delta
+		vel_y -= friction_y
+		direction.y = max(vel_y, 0)
+	if direction.y < 0:
+		friction_y = acc * delta
+		vel_y += friction_y
+		direction.y = min(vel_y, 0)
