54a4f77aaea6f25aa39e809374d468527be28070
AArthurius <arthurpennarey@gmail.com>
2024-09-08 23:13:52 -0300

    Update scout.gd

--- Scripts/Entities/Enemies/scout.gd
+++ Scripts/Entities/Enemies/scout.gd
@@ -5,26 +5,26 @@ extends CharacterBody2D
 @onready var hull: AnimatedSprite2D = $Sprites/hull
 
 var dead:bool = false
+var player_direction: Vector2
+var direction: Vector2
 
-const SPEED = 300.0
-
+const ACC = 150
+const MAX_SPEED = 200
 
 func _process(delta: float) -> void:
 	if dead:
 		return
 	
 	look_at(player.position)
-	pass
+	player_direction = (player.position - global_position).normalized()
 
 func _physics_process(delta: float) -> void:
 	if dead:
 		velocity = Vector2(0, 0)
 		return
 	
-	var player_direction = (player.position - global_position).normalized()
 	
-	velocity = player_direction * SPEED * delta
-	move_and_slide()
+	handle_movement(delta)
 
 func kill():
 	dead = true
@@ -38,3 +38,40 @@ func kill():
 func _on_hull_animation_finished() -> void:
 	if $Sprites/hull.animation == "death":
 		queue_free()
+
+func handle_movement(delta: float) -> void:
+	if player_direction:
+		direction += player_direction * ACC * delta
+	else:
+		friction(delta)
+	
+	direction.x = clamp(direction.x, -MAX_SPEED, MAX_SPEED)
+	direction.y = clamp(direction.y, -MAX_SPEED, MAX_SPEED)
+	
+	velocity = direction 
+	move_and_slide()
+
+func friction(delta):
+	var friction_x: int = 0
+	var friction_y: int = 0
+	var vel_x: int = direction.x
+	var vel_y: int = direction.y
+	
+	
+	if direction.x > 0:
+		friction_x = ACC * delta
+		vel_x -= friction_x
+		direction.x = max(vel_x, 0)
+	if direction.x < 0:
+		friction_x = ACC * delta
+		vel_x += friction_x
+		direction.x = min(vel_x, 0)
+	
+	if direction.y > 0:
+		friction_y = ACC * delta
+		vel_y -= friction_y
+		direction.y = max(vel_y, 0)
+	if direction.y < 0:
+		friction_y = ACC * delta
+		vel_y += friction_y
+		direction.y = min(vel_y, 0)
